e04783c479872806cd2a9f26b06377ab2506670f
nid-94 <86736538+nid-94@users.noreply.github.com>
2021-10-12 18:18:06 +0100

    adding bootstrap in navigation

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -4,7 +4,15 @@ const Footer = () => {
     return (
         <footer>
             <hr />
-            <p> this website is on work</p>
+            <p>
+                <b>
+                    this website is on work please be patient til we end
+                    building it
+                </b>{" "}
+            </p>
+            <p>
+                <i>enjoy your time with us .</i>{" "}
+            </p>
         </footer>
     );
 };
--- client/src/component/Navigation.js
+++ client/src/component/Navigation.js
@@ -1,9 +1,21 @@
 import React from "react";
-
+import { Nav, Container, Navbar } from "react-bootstrap";
 const Navigation = () => {
     return (
         <header>
-            <h1>navbar</h1>
+            <Navbar bg="info" variant="light">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        Welcome to mern Satck
+                    </Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Pages</Nav.Link>
+                        <Nav.Link href="#pricing">About</Nav.Link>
+                        <Nav.Link href="#pricing">Contact</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
         </header>
     );
 };
